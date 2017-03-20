519a6916860374f1dcbc502a13da0870a9937280
Ravi Yadav <rickcarter1806@gmail.com>
2017-03-20 20:04:27 +0530

    Routing

---
+++ 014routing/app.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs  = require('fs');
+
+http.createServer(function (req,res) {
+
+    if(req.url === '/') {
+        // res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname+ '/index.html','utf8').pipe(res);
+    }
+
+    else if(req.url === '/api') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        var obj = {
+            firstName: 'Ravi',
+            lastName: 'Yadav'
+        };
+        res.end(JSON.stringify(obj));
+    } else
+    {
+        res.writeHead(404);
+        res.end('Page not Found');
+    }
+}).listen(1337, '127.0.0.1');
