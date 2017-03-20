f6612251d714ccf251c41fe6919b18b4627fbe8e
Ravi Yadav <rickcarter1806@gmail.com>
2017-03-20 19:28:54 +0530

    Outputting JSON data

---
+++ 013outputtingJSON/app.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs  = require('fs');
+
+http.createServer(function (req,res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var obj = {
+        firstName: 'Ravi',
+        lastName: 'Yadav'
+    };
+    res.end(JSON.stringify(obj));
+}).listen(1337, '127.0.0.1');
