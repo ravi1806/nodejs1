908ec478286397ece8bdb70fb0e60ebe589a6f3f
Ravi Yadav <rickcarter1806@gmail.com>
2017-03-20 18:14:46 +0530

    Create a Server

--- 010createAserver/app.js
+++ 010createAserver/app.js
@@ -1,1 +1,7 @@
 var http = require(`http`);
+
+http.createServer(function (req,res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'}); /* What we give back is plain text */
+    res.end('Hello World\n'); /* Last thing that We want to send */
+}).listen(1337,'127.0.0.1'); /* Listen to the port/ports, localhost -> 127.0.0.1  */
+
